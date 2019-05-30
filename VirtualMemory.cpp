27a6694e44b73b99283a42be9709be6b689e0c4b
sineen <sineen.nawatha@mail.huji.ac.il>
2019-05-30 16:49:02 +0300

    init functions todo

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -47,7 +47,7 @@ pageOffset translateToPage(const uint64_t& virtualAddress);
  */
 
 //todo
-// maybe give it root ?
+// maybe give it root ?  maybe create a DFS function alone
 int64_t nextUnusedFram();
 /*
  * traversing over the tress, DFS  and returns the umber of fram that was not used
@@ -64,33 +64,25 @@ uint64_t minimalCyclicDistance(uint64_t pageToFill);
 /*
  * return the page number with the inimal cyclic distance from the pageToFill
  */
-void virtualToPhysical(uint64_t virtualAddress, uint64_t array[TABLES_DEPTH]) {
-	word_t current_address = 0;
-	word_t new_address;
-	int64_t current_index;
-	uint64_t left_number = virtualAddress;
-	for(unsigned i = 0; i < TABLES_DEPTH; ++i) {
-		current_index = left_number >> (uint64_t )OFFSET_WIDTH * (TABLES_DEPTH-i);
-		left_number %= (uint64_t )pow(2, OFFSET_WIDTH * ((uint64_t )TABLES_DEPTH-i));
-		PMread(current_index + current_address * PAGE_SIZE, &new_address);
-
-		if(!new_address){
-			for(unsigned i = 1; i <NUM_FRAMES; ++i){
-				word_t val;
-				for(unsigned j = 0; j < PAGE_SIZE; ++j){
-					PMread(i * PAGE_SIZE + j, &val)
-					if(val != 0 && current_address != i) {
-						next_frame = i;
-					}
-				}
-			}
-			new_frame = -1
-		}
-
-	}
-}
 
 
+//todo
+uint64_t toPhysical(pageOffsetPair pair);
+/*
+ * maps page fro pair to frame then connects it to ofset adn returns that value which is teh
+ * physical address
+ */
+
+
+int evict(uint64_t newAddress);
+/*
+ * if physical memory full get fram with max cyclic distance then deletes that fram delete link
+ * to it and creates it with empty valued to where i need to add it
+ */
+
+int deleteFrame();
+
+int createFrame();
 
 void VMinitialize() {
     clearTable(0);
